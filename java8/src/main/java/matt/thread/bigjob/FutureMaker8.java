49a41f85454c47797cb5eed6b24b497273643353
Matt Dailey <matthew.dailey1@gmail.com>
2016-01-29 20:00:03 -0500

    FutureMaker8 has combined method that's not very readable

--- java8/src/main/java/matt/thread/bigjob/FutureMaker8.java
+++ java8/src/main/java/matt/thread/bigjob/FutureMaker8.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.lang.StringUtils;
 
 import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 
@@ -30,5 +31,17 @@ public class FutureMaker8 implements FutureMaker {
   /** Create AsyncFunction that passes input to {@link #createSecondStep(String)} */
   public AsyncFunction<String, String> createFirstToSecondFunction() {
     return ((String s) -> createSecondStep(s));
+  }
+
+  /**
+   * Combine the other functions in the most lambda way possible and with the most
+   * type inference possible, but not necessarily the most readable :/
+   */
+  public ListenableFuture<String> makeComposed(List<String> idChunk) {
+    return Futures.transform(
+            executor.submit(
+                    () -> StringUtils.join(idChunk, "")),
+            (String s) -> s.toUpperCase(),
+            executor);
   }
 }
